7fc81fb4a04df6c3cfbcf6cbea2999b7d4ddcd56
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-06-16 14:11:44 +0200

    feat : connect nestjs app to mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
